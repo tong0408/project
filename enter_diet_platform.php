b25385839d7336115e50829c1c0b4a8e50030612
yi0920 <101317125+yi0920@users.noreply.github.com>
2022-08-05 01:23:48 +0800

    Update enter_diet_platform.php

--- enter_diet_platform.php
+++ enter_diet_platform.php
@@ -9,39 +9,15 @@
 <link href="image/logo.png" rel="icon" type="image/x-icon" />
 <link rel="stylesheet" href="css/bootstrap-3.3.7.css" type="text/css">
 <link rel="stylesheet" href="css/w3.css">
+<link rel="stylesheet" href="css/mine.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-<style>
-	body,h1,h2,h3,h4,h5,h6 {font-family: "微軟正黑體", sans-serif}
-	
-	body, html {
-		height: 100%;
-		line-height: 1.8;
-	}
-	input[type=text]{margin:10px 0px 10px 0px; width:50%;}
-	button{margin:0px 10px 0px 10px;}
-	.form1{
-		background-color: #FFD79B;
-		border-radius:30px;
-		width:70%;
-		margin:auto;
-		padding:30px 0px;
-		text-align:center;
-	}
-	
-	.btn{
-		border-radius:10px;
-		background-color: #FFF;
-	}
-	
-	.btn:hover{
-		background-color: #FFB03B;
-	}
+<style>	
 	td{height:50px;}
 </style>
 </head>
 <body>
-
+<a href="daily_diet.html"><button class="btn1 return">返回</button></a>
     <div class="form1">
 		食品：<input type="text" name="search_text" id="search_text" placeholder="請輸入搜尋文字" class="form-control" style="display:inline-block">
 		<div id="search_result"></div>
