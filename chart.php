d286b959d365666de30e824415e32a044c880b1d
tong0408 <100017187+tong0408@users.noreply.github.com>
2022-08-12 23:36:51 +0800

    Add files via upload

--- chart.php
+++ chart.php
@@ -74,49 +74,45 @@
 <button class="btn">月</button>
 <button class="btn">周</button>
 <div class="container1">
-<button class="btn2">全榖雜糧類</button>
-<button class="btn2">豆魚蛋肉類</button>
-<button class="btn2">乳品類</button>
-<button class="btn2">蔬菜類</button>
-<button class="btn2">水果類</button>
-<button class="btn2">油脂與堅果種子類</button>
+<button class="btn2" id="1">全榖雜糧類</button>
+<button class="btn2" id="2">豆魚蛋肉類</button>
+<button class="btn2" id="3">乳品類</button>
+<button class="btn2" id="4">蔬菜類</button>
+<button class="btn2" id="5">水果類</button>
+<button class="btn2" id="6">油脂與堅果種子類</button>
 <canvas id="myChart" ></canvas>
 </div>
 </div>
 <?php
-    //連接歷史紀錄資料表（限制日期）
+    //連接歷史紀錄資料表
     $link = new PDO('mysql:host=' . $hostname . ';dbname=' . $database . ';charset=utf8', $username, $password);
     $query = "SELECT `dishID` FROM `history` WHERE `UID`='$userID' limit 7";
-    $result = $link->query($query);	
+    $result = $link->query($query);
+	
     foreach($result as $row){
-        if($row["date"]!=null){
-			$dishID = $row['dishID'];
-        }
+        
+		$dishID = $row['dishID'];
+
     //取得所有需要的資料
 		
-		//取得菜ID使用的食材ID
+		//取得dishID使用的iID&portion
 		$query = "SELECT `iID`, `portion` FROM recipe where dishID='$dishID'";
 		$re = $link->query($query);
 		foreach ($re as $r){
 			$iID=$r['iID'];
-
-			//從食材ID取得食材類別ID和食材名稱<使用>
+			$portion=$r['portion'];
+			
+			//從iID取得NID和食材名稱<使用>
 			$query = "SELECT NID FROM ingredients where iID='$iID'";
 			$re = $link->query($query);
 			foreach ($re as $r){
-				$iID_NID=$r['NID'];
-
-				//從食材類別ID取得食材類別名稱<使用>
-				$query = "SELECT * FROM nutrient where NID='$iID_NID'";
-				$re = $link->query($query);
-				foreach ($re as $r){
-					$iID_NID_Name=$r['category'];	
-				}							
+				$iID_NID=$r['NID'];						
 			}
 		}
 	}
 ?>
 <script>
+
 //日期自動變化
 var Today=new Date();
 var month=Today.getMonth()+1;
@@ -126,52 +122,90 @@ for(var i=1;i<32;i++){
 	date.push(i);
 }
 //改善的地方為:如果是1號與31號 怎麼半?
-	var mmdd1=Today.getMonth()+1+'/'+(Today.getDate()-6);
-	var mmdd2=Today.getMonth()+1+'/'+(Today.getDate()-5);
-	var mmdd3=Today.getMonth()+1+'/'+(Today.getDate()-4);
-	var mmdd4=Today.getMonth()+1+'/'+(Today.getDate()-3);
-	var mmdd5=Today.getMonth()+1+'/'+(Today.getDate()-2);
-	var mmdd6=Today.getMonth()+1+'/'+(Today.getDate()-1);
-	var mmdd7=Today.getMonth()+1+'/'+Today.getDate();
+	var mmdd1=Today.getMonth()+1+'-'+(Today.getDate()-6);
+	var mmdd2=Today.getMonth()+1+'-'+(Today.getDate()-5);
+	var mmdd3=Today.getMonth()+1+'-'+(Today.getDate()-4);
+	var mmdd4=Today.getMonth()+1+'-'+(Today.getDate()-3);
+	var mmdd5=Today.getMonth()+1+'-'+(Today.getDate()-2);
+	var mmdd6=Today.getMonth()+1+'-'+(Today.getDate()-1);
+	var mmdd7=Today.getMonth()+1+'-'+Today.getDate();
 
+function show(){
+	const ctx = document.getElementById('myChart').getContext('2d');
+	const myChart = new Chart(ctx, {
+		type: 'bar',
+		data: {
+			labels: [mmdd1, mmdd2, mmdd3, mmdd4, mmdd5, mmdd6,mmdd7 ],//改日期
+			datasets: [{
+				label: '營養素圖表',
+				data: [12, 19, 13, 5, 12, 13,15],//改數值
+				backgroundColor: [
+					'rgba(255, 99, 132, 0.2)',
+					'rgba(54, 162, 235, 0.2)',
+					'rgba(255, 206, 86, 0.2)',
+					'rgba(75, 192, 192, 0.2)',
+					'rgba(153, 102, 255, 0.2)',
+					'rgba(255, 159, 64, 0.2)',
+					'rgba(220, 31, 224, 0.2)'
+				],
+				borderColor: [
+					'rgba(255, 99, 132, 1)',
+					'rgba(54, 162, 235, 1)',
+					'rgba(255, 206, 86, 1)',
+					'rgba(75, 192, 192, 1)',
+					'rgba(153, 102, 255, 1)',
+					'rgba(255, 159, 64, 1)',
+					'rgba(220, 31, 224, 1)'
+				],
+				borderWidth: 1
+			}]
+		},
+		options: {
+			scales: {
+				y: {
+					beginAtZero: true
+				}
+			}
+		}
+	});
+}
 
-const ctx = document.getElementById('myChart').getContext('2d');
-const myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: [mmdd1, mmdd2, mmdd3, mmdd4, mmdd5, mmdd6,mmdd7 ],//改日期
-        datasets: [{
-            label: '營養素圖表',
-            data: [12, 19, 13, 5, 12, 13,15],//改數值
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)',
-                'rgba(220, 31, 224, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)',
-                'rgba(220, 31, 224, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
-});
+window.onload=function(){
+	//onclick button
+	var n1=document.getElementById("1");
+	var n2=document.getElementById("2");
+	var n3=document.getElementById("3");
+	var n4=document.getElementById("4");
+	var n5=document.getElementById("5");
+	var n6=document.getElementById("6");
+	
+	n1.onclick=function(){
+		show();
+		return false;
+	}
+	n2.onclick=function(){
+		show();
+		return false;
+	}
+	n3.onclick=function(){
+		show();
+		return false;
+	}
+	n4.onclick=function(){
+		show();
+		return false;
+	}
+	n5.onclick=function(){
+		show();
+		return false;
+	}
+	n6.onclick=function(){
+		show();
+		return false;
+	}
+}
+			
+
 </script>
 </body>
 </html>
