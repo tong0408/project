a743b7131824ac35197ea645fde14fea8008a622
hsuxuu <93756656+hsuxuu@users.noreply.github.com>
2022-10-17 22:47:40 +0800

    1017 修正多疾病判斷目標值

--- menu.php
+++ menu.php
@@ -394,7 +394,7 @@
 	}
 
 	#第一個疾病為糖尿病+其他疾病時
-	if($user_disease="糖尿病"){
+	if($user_disease=="糖尿病"){
 		#糖尿病+肝硬化疾病
 		if($user_disease2=="肝硬化" || $user_disease3=="肝硬化" || $user_disease4=="肝硬化" ||
 		$user_disease5=="肝硬化" || $user_disease6=="肝硬化" || $user_disease7=="肝硬化"){
@@ -419,7 +419,19 @@
 	$user_disease5=="肝硬化" || $user_disease6=="肝硬化" || $user_disease7=="肝硬化") {
 	$goal_fat=($user_cal*0.25)/9;
 	$goal_fat=round($goal_fat);
-}
+	}
+
+	#抓取資料庫獲取目前吃的營養素
+	$now_category_1=0;
+	$now_category_2=0;
+	$now_category_3=0;
+	$now_category_4=0;
+	$now_category_5=0;
+	$now_category_6=0;
+	$now_category_7=0;
+	$now_glyco=0;
+	$now_fat=0;
+	$now_protein=0;
 
     echo "<h3><b>".$Name." 您好！</b></h3><br>
 	您的疾病為：「";
@@ -429,80 +441,104 @@
 	if($user_disease4!=0){echo "、".$user_disease4;}
 	if($user_disease5!=0){echo "、".$user_disease5;}
 	if($user_disease6!=0){echo "、".$user_disease6;}
-	echo "」，目前BMI為：".$user_BMI."，一天建議攝取".($goal_glyco*4+$goal_fat*9+$goal_protein*4)."大卡。<br>
-    每日建議攝取量：全榖雜糧類：".$category_1."份  蛋豆魚肉類：".$category_2."份 乳品類：".$category_3."份  蔬菜類：".$category_4."份  水果類：".$category_5."份 油脂類：".$category_6."份  堅果種子類：".$category_7."份<br>
-    目標醣類：$goal_glyco g、脂質：$goal_fat g、蛋白質：$goal_protein g<br><br>
+	echo "」，目前BMI為：".$user_BMI."。一天建議攝取".($goal_glyco*4+$goal_fat*9+$goal_protein*4)."大卡。<br>
+    <hr><h4><b>每日建議攝取量－目標</b></h4>
+	<table width='80%'>
+	<tr>
+	<td><b>全榖雜糧類</b></td>
+	<td><b>蛋豆魚肉類</b></td>
+	<td><b>乳品類</b></td>
+	<td><b>蔬菜類</b></td>
+	<td><b>水果類</b></td>
+	<td><b>油脂類</b></td>
+	<td><b>堅果種子類</b></td>
+	<td><b>醣類</b></td>
+	<td><b>脂質</b></td>
+	<td><b>蛋白質</b></td>
+	</tr>
+	<tr>
+	<td><b>$now_category_1/$category_1 份</b></td>
+	<td><b>$now_category_2/$category_2 份</b></td>
+	<td><b>$now_category_3/$category_3 份</b></td>
+	<td><b>$now_category_4/$category_4 份</b></td>
+	<td><b>$now_category_5/$category_5 份</b></td>
+	<td><b>$now_category_6/$category_6 份</b></td>
+	<td><b>$now_category_7/$category_7 份</b></td>
+	<td><b>$now_glyco/$goal_glyco g</b></td>
+	<td><b>$now_fat/$goal_fat g</b></td>
+	<td><b>$now_protein/$goal_protein g</b></td>
+	</tr>
+	</table>
+	<hr>
 	<b><font size='5'>以下推薦幾道菜單讓您選擇！</font></b><br>";
 ?>
-			<div class="box" id="left">
-				<table width="200">				
-				<?PHP			
-					$link = new PDO('mysql:host=' . $hostname . ';dbname=' . $database . ';charset=utf8', $username, $password);
-					$query = "SELECT count(dishID) FROM t_menugetid ";
-					$result = $link->query($query);
-					$count = $result->fetchColumn();
+	<div class="box" id="left">
+		<table width="200">				
+			<?PHP			
+				$link = new PDO('mysql:host=' . $hostname . ';dbname=' . $database . ';charset=utf8', $username, $password);
+				$query = "SELECT count(dishID) FROM t_menugetid ";
+				$result = $link->query($query);
+				$count = $result->fetchColumn();
+				if($count==0){
+					$query = "SELECT DISTINCT recipe.dishID,dish.dishname,dish.method FROM recipe INNER JOIN dish on recipe.dishID = dish.ID LIMIT 9";
+					$result = $link->query($query);	
 
-					if($count==0){
-						$query = "SELECT DISTINCT recipe.dishID,dish.dishname,dish.method FROM recipe INNER JOIN dish on recipe.dishID = dish.ID LIMIT 9";
+					//取得所有需要的資料
+					foreach ($result as $row){					
+						//取得欄位數量
+						$dishID = $row['dishID'];
+						$dishname = $row['dishname'];
+						$method = $row['method'];
+							
+						//顯示結果
+						echo '<tr>'.
+						//菜名
+						'<td class="dish" onClick="showtitle(event);" title="'.nl2br($method).'" id="'.$dishID.'">'.$dishname.'</td></tr>';
+					}
+				} 
+				else {
+					$query = "SELECT dishID FROM t_menugetid ";
+					$result = $link->query($query);
+					foreach ($result as $r){
+						
+						$t_dishID=$r['dishID'];
+
+						$query = "SELECT DISTINCT recipe.dishID,dish.dishname,dish.method FROM recipe INNER JOIN dish on recipe.dishID = dish.ID WHERE `recipe`.dishID=$t_dishID";
 						$result = $link->query($query);	
-	
-						//取得所有需要的資料
-						foreach ($result as $row){					
+
+						foreach ($result as $row){				
 							//取得欄位數量
 							$dishID = $row['dishID'];
 							$dishname = $row['dishname'];
 							$method = $row['method'];
-								
 							//顯示結果
 							echo '<tr>'.
 							//菜名
 							'<td class="dish" onClick="showtitle(event);" title="'.nl2br($method).'" id="'.$dishID.'">'.$dishname.'</td></tr>';
 						}
-					}else{
-						$query = "SELECT dishID FROM t_menugetid ";
-						$result = $link->query($query);
-						foreach ($result as $r){
-							
-							$t_dishID=$r['dishID'];
-
-							$query = "SELECT DISTINCT recipe.dishID,dish.dishname,dish.method FROM recipe INNER JOIN dish on recipe.dishID = dish.ID WHERE `recipe`.dishID=$t_dishID";
-							$result = $link->query($query);	
-
-							foreach ($result as $row){				
-								//取得欄位數量
-								$dishID = $row['dishID'];
-								$dishname = $row['dishname'];
-								$method = $row['method'];
-
-								//顯示結果
-								echo '<tr>'.
-								//菜名
-								'<td class="dish" onClick="showtitle(event);" title="'.nl2br($method).'" id="'.$dishID.'">'.$dishname.'</td></tr>';
-							}
-						}
 					}
-					//刪除t_menugetid的資料
-					$sql = "DELETE FROM `t_menugetid` ";
-					// 用mysqli_query方法執行(sql語法)將結果存在變數中
-					$count = $link->exec($sql);				
-				?>
-				<tr><td class="check"><a href="menu_button.php">查看更多</a></td></tr>
-				</table>
+				}
+				//刪除t_menugetid的資料
+				$sql = "DELETE FROM `t_menugetid` ";
+				// 用mysqli_query方法執行(sql語法)將結果存在變數中
+				$count = $link->exec($sql);				
+			?>
+		<tr><td class="check"><a href="menu_button.php">查看更多</a></td></tr>
+		</table>
+	</div>
+	<div class="container2">
+		<div class="box" id="right">				
+			點選菜名查看料理方式<br><br>
+				<div id="getdish">&nbsp;</div>
+				<div class="get" id="gettitle">&nbsp;</div>
 			</div>
-			<div class="container2">
-				<div class="box" id="right">				
-					點選菜名查看料理方式<br><br>
-					<div id="getdish">&nbsp;</div>
-					<div class="get" id="gettitle">&nbsp;</div>
-				</div>
-				<div class="box" id="right">
-					<div class="get" id="getid"><br></div>
-				</div>
+			<div class="box" id="right">
+				<div class="get" id="getid"><br></div>
 			</div>
+		</div>
+	</div>
 </div>
 <br>
-依據缺少的營養素去做推薦菜單<br>
-（去比對資料庫內該營養素較相符合的，如果吃了導致其他營養素過量，也要篩選）<br>
 
 </body>
 <script>
