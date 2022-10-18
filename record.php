76f53d446a58970aa9846a810f133fe8a7fee4b1
tong0408 <100017187+tong0408@users.noreply.github.com>
2022-10-18 22:23:47 +0800

    1018修改
    record.php->增加user_histroy_modify資料表有資料時顯示

--- record.php
+++ record.php
@@ -57,7 +57,6 @@
 				$get_date=isset($_POST["get_date"]) ? $_POST["get_date"] : $today;
 
 				//連接歷史紀錄資料表（限制日期）
-				$link = new PDO('mysql:host=' . $hostname . ';dbname=' . $database . ';charset=utf8', $username, $password);
 				$query = "SELECT * FROM `history` WHERE `UID`='$userID' and `date`='$get_date'";
 				$result = $link->query($query);	
 				
@@ -114,16 +113,111 @@
 									'<td style="height:50px;">'.$iID_Name.'</td>'.
 									//克數
 									'<td style="height:50px;">'.$iportion.'</td></tr>';									
-								}							
-							}						
+							}							
+						}						
+					}
+				}
+
+				//放user_histroy_modify的dishID
+				$t_dishID=array();
+				$t_iID=array(array());
+				$t_iportion=array(array());
+				$t_portion=array();
+				//連接user_histroy_modify（限制日期）
+				$query = "SELECT * FROM `user_histroy_modify` WHERE `UID`='$userID' and `date`='$get_date'";
+				$result = $link->query($query);
+
+				//如果dishID不同就放t_dishID陣列，拿資料
+				$a=0;
+				$b=0;
+				$c=0;
+				$d=0;
+				foreach ($result as $row){
+					if($a==0){
+						$t_dishID[$a]=$row['dishID'];
+						$t_portion[$a]=$row["portion"];
+						$c=1;
+					}else if($t_dishID[$a-1]!=$row['dishID']){
+						$t_dishID[$a]=$row['dishID'];
+						$t_portion[$a]=$row["portion"];
+						$c=1;
+					}
+					//取得菜ID使用的食材ID及克數
+					if($a==0){
+						$t_iID[$d][$b]=$row["iID"];
+						$t_iportion[$d][$b]=$row["iportion"];
+						$b=$b+1;
+					}else if($t_dishID[$a-1]==$row['dishID']){
+						$t_iID[$d][$b]=$row["iID"];
+						$t_iportion[$d][$b]=$row["iportion"];
+						$b=$b+1;
+					}else if($t_dishID[$a-1]!=$row['dishID']){
+						$d=$d+1;
+						$b=0;
+						$t_iID[$d][$b]=$row["iID"];
+						$t_iportion[$d][$b]=$row["iportion"];
+					}
+					if($c==1){
+						$a=$a+1;
+					}
+					$c=0;
+				}
+				
+				//取得所有需要的資料
+				for($i=0;$i<count($t_dishID);$i++){
+					
+					//取得欄位數量
+					$dishID = $row['dishID'];
+					$count = $link->prepare("SELECT * FROM user_histroy_modify WHERE `dishID`='$dishID[$i]'");   
+					$count->execute();   
+					$count_rows=$count->rowCount(); 
+					
+					//顯示結果
+					echo '<tr>'.
+					//時間
+					'<td style="height:50px;" rowspan="'.$count_rows.'">'.$row["time"].'</td>';
+					
+					//從菜ID取得菜名稱<使用>
+					$query = "SELECT * FROM dish where ID='$dishID[$i]'";
+					$re = $link->query($query);
+					foreach ($re as $r){
+						$dish_Name=$r['dishName'];
+					}
+					//菜名
+					echo '<td style="height:50px;" rowspan="'.$count_rows.'">'.$dish_Name.'</td>
+					      <td style="height:50px;" rowspan="'.$count_rows.'">'.$t_portion[$i].'</td>';
+
+					//連接user_histroy_modify（限制日期）
+					$query = "SELECT * FROM `user_histroy_modify` WHERE `UID`='$userID' and `dishID`='$dishID[$i]'";
+					$result = $link->query($query);
+
+					foreach ($result as $rw){
+						//取得菜ID使用的食材ID及克數
+						$t_iID=$rw["iID"];
+						$t_iportion=$rw["iportion"];
+
+						$query = "SELECT * FROM ingredients where iID='$t_iID'";
+						$re = $link->query($query);
+						foreach ($re as $r){
+							$iID_NID=$r['NID'];
+							$iID_Name=$r['name'];
+							//從食材類別ID取得食材類別名稱<使用>
+							$query = "SELECT * FROM nutrient where NID='$iID_NID'";
+							$re = $link->query($query);
+							foreach ($re as $r){
+								$iID_NID_Name=$r['category'];
+								//食材
+								echo '<td style="height:50px;">'.$iID_NID_Name.'</td>'.
+								//分類
+								'<td style="height:50px;">'.$iID_Name.'</td>'.
+								'<td style="height:50px;">'.$t_iportion.'</td></tr>';					
+							}							
 						}
 					}
+				}
 					echo '</tbody>';
-			
 			?>
 		</table>
 	</div>
- 
 </body>
-
 </html>
