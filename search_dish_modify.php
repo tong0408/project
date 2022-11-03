9f269fc9f4dcbc28ed782f97682a0cdf06b50f82
tong0408 <100017187+tong0408@users.noreply.github.com>
2022-11-04 02:43:19 +0800

    1104
    search_dish_modify.php->客製化料理，想要再修改，會呈現正確的頁面(不會在一直往下增加)

--- search_dish_modify.php
+++ search_dish_modify.php
@@ -7,18 +7,33 @@
     $userid= $_SESSION['userID'];//userid
 	$dishname=$_SESSION['dish_Name']; //修改的菜名
     $dishid=$_SESSION['dishID'];//修改的菜id
-	$ingredients = isset($_POST["ingredients"]) ? $_POST["ingredients"] : null; //資料庫內抓到的食材名稱
-    //$newingredients = isset($_POST["newingredients"]) ? $_POST["newingredients"] : null; //修改的食材名稱
+	$ingredients = isset($_POST["ingredients"]) ? $_POST["ingredients"] : null; //修改的食材名稱
 	$new_portion = isset($_POST["new_portion"]) ? $_POST["new_portion"] : null; //修改的份量
 
+    
+    //先查看是否改過
+    $query = "SELECT count(ID) FROM t_user_histroy_modify WHERE `UID`='$userid' AND `dishID`='$dishid' ";
+    $result = $link->query($query);
+    $count = $result->fetchColumn();
+
+    //代表有改過
+    if($count>0){
+        //刪除t_user_histroy_modify該道料理
+        $sql = "DELETE FROM `t_user_histroy_modify` WHERE `UID`='$userid' and `dishID`='$dishid'";
+        // 用mysqli_query方法執行(sql語法)將結果存在變數中
+        $count = $link->exec($sql);
+    }
 	for($i=0;$i<count($ingredients)-1;$i++){
-        //ECHO $new_portion[$i].$i;
+        
+        //ECHO count($ingredients).$ingredients[$i].",".$new_portion[$i];
+
         //搜尋食材名稱
         $query = "SELECT iID FROM `ingredients` WHERE `name`='$ingredients[$i]'";
-		$result = $link->query($query);
+        $result = $link->query($query);
+
         foreach($result as $row){
             $iID=$row["iID"];
-
+        
             //新增至t_user_histroy_modify
             $query = "INSERT INTO `t_user_histroy_modify`(`UID`, `dishID`, `iID`, `portion`) 
             VALUES('$userid',$dishid,$iID,$new_portion[$i])";
